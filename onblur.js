6488a893ae499bbae9d511dfa3e23dcfcc392666
amithardikar <35449757+amithardikar@users.noreply.github.com>
2018-01-16 12:42:07 +0530

    16th jan

---
+++ onblur.js
@@ -0,0 +1,16 @@
+function dog()
+{
+/*var dog={name:"lab",weight:33};
+
+if(dog.name=="lab")
+{
+	alert("ur food is ready");
+}*/
+var d1=document.getElementById("amit");
+
+if(d1.value=="lab")
+{
+	alert("ur food is ready");
+}
+
+}
